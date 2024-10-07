18b691cb3b218bf3f12978f557011d26fd4ba40c
Christian <cmcarbajal05@gmail.com>
2024-10-07 16:36:56 -0400

    updated README and fixed minor bugs

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Readme Generator ![License](https://img.shields.io/badge/license-MIT-green.svg)
 ## Description
-Readme Generator
+This readme generator allows you to automatically create readme file after answering a few questions.
 ## Table of Contents 
 
 - [Installation](#installation)
@@ -10,15 +10,15 @@ Readme Generator
 - [Features](#features)
 
 ## Installation
-Readme Generator
+npm i
 
 ## Usage
-To initialize run Node index.js in your terminal
+To initialize run index.js in your terminal.
 
 ## Credits
 Find me at github.com/YamatoWanoKuni 
-Email:[cmarbajal05@gmail.com](mailto:cmcarbajal05@gmail.com)
-
+Email: [cmcarbajal05@gmail.com](mailto:cmcarbajal05@gmail.com)
+[Tutorial](https://youtu.be/jZlwT-xmcgU) Video
 ## License
 MIT
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -7,19 +7,11 @@ function renderLicenseBadge(license) {
   return '';
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title} ${renderLicenseBadge(data.license)}
 ## Description
-${data.title}
+${data.description}
 ## Table of Contents 
 
 - [Installation](#installation)
@@ -29,7 +21,7 @@ ${data.title}
 - [Features](#features)
 
 ## Installation
-${data.title}
+${data.dependencies}
 
 ## Usage
 ${data.usage}
